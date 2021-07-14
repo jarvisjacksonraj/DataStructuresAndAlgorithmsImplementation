0df1c8c0143f4092864011fdf0c90a3468bb4bd1
jarvisjacksonraj <53148568+jarvisjacksonraj@users.noreply.github.com>
2021-07-15 00:13:31 +0530

    DS ALGO Implementation 1.1

--- src/main/java/DS_ALGO/Algorithms/Sorting/SelectionSort.java
+++ src/main/java/DS_ALGO/Algorithms/Sorting/SelectionSort.java
@@ -7,7 +7,6 @@ import java.util.List;
 public class SelectionSort<T> {
     static Util obj = new Util();
 
-    @SuppressWarnings("unchecked")
     public void selectionSort(List<T> arr, int arrSize) {
         int minInd;
         for (int i = 0; i < arrSize; i++) {
@@ -16,20 +15,4 @@ public class SelectionSort<T> {
         }
     }
 
-    @SuppressWarnings("unchecked")
-    public static void main(String args[]) {
-        List<Integer> arrayIntegers = new ArrayList<>(Arrays.asList(5, 4, 1, 0, 5, 95, 4, -100, 200, 0));
-        List<Character> arrayCharacter = new ArrayList<>(Arrays.asList('v', 'a', 'g', 's', 'h', 'q', 'r', 'z'));
-
-        int arrSize = 10;
-        SelectionSort<Integer> selectionSort = new SelectionSort<>();
-        SelectionSort<Character> selectionSort2 = new SelectionSort<>();
-
-        selectionSort.selectionSort(arrayIntegers, arrSize);
-        selectionSort2.selectionSort(arrayCharacter, 8);
-
-        obj.printArray(arrayCharacter, 8);
-        obj.printArray(arrayIntegers, arrSize);
-
-    }
 }
